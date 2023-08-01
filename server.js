ff40f18fb554bda68d36215f0cd536d1885cbf5b
IncendioHawk <incendiohawk@gmail.com>
2023-08-01 21:49:06 +0100

    Solved issue with session timeout

--- server.js
+++ server.js
@@ -7,11 +7,11 @@ const indexRouter = require("./routes/index")
 const cookieParser = require("cookie-parser")
 const Session = require("./models/sessionSchema")
 
-const sessionTimeoutTime = 10000
-// const sessionTimeoutTime = 300000
+const sessionTimeoutTime = 300000
 
 app.use(express.static(__dirname + "/public"))
 app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 app.use(cookieParser())
 app.set("view engine", "ejs")
 
@@ -36,15 +36,14 @@ app.get("/interactedWithPage", async (req, res) => {
   }
   if (req.query.boolean === "false") {
     const session = await Session.findOne({ sessionId: req.cookies["_session_ID"] })
-    if (session == null)
-      return res.render("logout-success", { message: "Session not found, please log in" })
+    if (session == null) return res.send({ message: "Session not found, please log in" })
     if (session.updatedAt > Date.now() - sessionTimeoutTime) return
     res.clearCookie("_session_ID")
-    res.render("logout-success", {
+    res.send({
       message: `You have been inactive for ${(sessionTimeoutTime / 60000).toFixed(
         3
       )} minutes. Please sign in again to keep using this service.`,
-    }) //fix this not rendering the page
+    })
     console.log(await Session.findOneAndDelete({ sessionId: session.sessionId }))
   }
 })
